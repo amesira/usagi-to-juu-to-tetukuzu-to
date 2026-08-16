7d8d3a39cf77e287ad134b41e834d1e0ef6a5bf4
amesira <miuktmr@gmail.com>
2026-08-16 22:37:29 +0900

    PlayerMove関連のコード整理

--- Source/Game/ActorBehavior/Player/P10_Locomotion/Move/player_move_motor.cpp
+++ Source/Game/ActorBehavior/Player/P10_Locomotion/Move/player_move_motor.cpp
@@ -23,10 +23,8 @@ void PlayerMoveMotor::UpdateMove_Motor(PlayerMoveContext& context, const PlayerM
             context.runtimeState.m_physicsVelocity.y = -0.1f;
         }
         else {
-            float gravityScale = 1.0f; // 重力のスケール（必要に応じて調整可能）
-
             float normalizedYVelocity = context.runtimeState.m_physicsVelocity.y / 0.1f;
-            gravityScale = context.settings().gravityScale.Evaluate(normalizedYVelocity);
+            float gravityScale = context.settings().gravityScale.Evaluate(normalizedYVelocity);
 
             // 重力加速度を計算する
             float gravityAcceleration = -9.81f * gravityScale;
@@ -40,37 +38,60 @@ void PlayerMoveMotor::UpdateMove_Motor(PlayerMoveContext& context, const PlayerM
         if (!context.runtimeState.m_isGrounded) {
             moveSpeed *= context.settings().airSpeedMultiplier;
         }
-        XMFLOAT3 inputVelocity = MiMath::Multiply(intent.moveDirection, moveSpeed);
-        inputVelocity.y = 0.0f;
+        m_inputVelocity = MiMath::Multiply(intent.moveDirection, moveSpeed);
+        m_inputVelocity.y = 0.0f;
 
         // 移動入力がある場合は通常の平滑化時間、移動入力がない場合は停止時の平滑化時間を使用する
-        float smoothTime = context.settings().smoothTime;
-        if (intent.moveInputMagnitude < 0.01f) {
-            smoothTime = context.runtimeState.m_isGrounded ? context.settings().stopSmoothTime : context.settings().stopAirSmoothTime;
-        }
-        else {
-            smoothTime = context.runtimeState.m_isGrounded ? context.settings().smoothTime : context.settings().airSmoothTime;
-        }
+        float smoothTime = CalculateSmoothTime(context);
 
-        // SmoothDampを使用して、現在の速度から目標速度に向かって平滑化する
-        float desiredVelocityX = MiMath::SmoothDamp(
-            context.runtimeState.m_controlVelocity.x, 
-            inputVelocity.x, 
-            m_velocitySmoothDamp.x, // REF: 速度保持変数を参照渡し
-            smoothTime, 
-            deltaTime);
-
-        float desiredVelocityZ = MiMath::SmoothDamp(
-            context.runtimeState.m_controlVelocity.z, 
-            inputVelocity.z, 
-            m_velocitySmoothDamp.z, // REF: 速度保持変数を参照渡し
-            smoothTime, 
-            deltaTime);
-
+        // === 目標速度を更新する ===
+        UpdateDesiredVelocity(context, smoothTime, deltaTime);
+        
         // === ControlVelocityを更新する ===
-        context.runtimeState.m_controlVelocity.x = desiredVelocityX;
-        context.runtimeState.m_controlVelocity.z = desiredVelocityZ;
+        context.runtimeState.m_controlVelocity.x = m_desiredVelocity.x;
+        context.runtimeState.m_controlVelocity.z = m_desiredVelocity.z;
+    }
+}
+
+/// @brief 現在の状況に応じた平滑化時間を計算する
+float PlayerMoveMotor::CalculateSmoothTime(PlayerMoveContext& context)
+{
+    bool stopInput = (MiMath::Length(m_inputVelocity) < 0.01f);
+
+    if (context.runtimeState.m_isGrounded && !stopInput) {
+        return context.settings().smoothTime;
+    }
+    else if (!context.runtimeState.m_isGrounded && !stopInput) {
+        return context.settings().airSmoothTime;
+    }
+    else if (context.runtimeState.m_isGrounded && stopInput) {
+        return context.settings().stopSmoothTime;
+    }
+    else if (!context.runtimeState.m_isGrounded && stopInput) {
+        return context.settings().stopAirSmoothTime;
     }
 
+    return context.settings().smoothTime;
+}
 
+/// @brief 滑らかにするための目標速度を計算する
+void PlayerMoveMotor::UpdateDesiredVelocity(PlayerMoveContext& context,float smoothTime, float deltaTime)
+{
+    // SmoothDampを使用して、現在の速度から目標速度に向かって平滑化する
+    float desiredVelocityX = MiMath::SmoothDamp(
+        m_desiredVelocity.x,
+        m_inputVelocity.x,
+        m_velocitySmoothDamp.x, // REF: 速度保持変数を参照渡し
+        smoothTime,
+        deltaTime);
+
+    float desiredVelocityZ = MiMath::SmoothDamp(
+        context.runtimeState.m_controlVelocity.z,
+        m_inputVelocity.z,
+        m_velocitySmoothDamp.z, // REF: 速度保持変数を参照渡し
+        smoothTime,
+        deltaTime);
+
+    m_desiredVelocity.x = desiredVelocityX;
+    m_desiredVelocity.z = desiredVelocityZ;
 }
--- Source/Game/ActorBehavior/Player/P10_Locomotion/Move/player_move_motor.h
+++ Source/Game/ActorBehavior/Player/P10_Locomotion/Move/player_move_motor.h
@@ -14,8 +14,17 @@ private:
     // SmoothDamp用の速度保持変数
     XMFLOAT3 m_velocitySmoothDamp = { 0.0f, 0.0f, 0.0f };
 
+    XMFLOAT3 m_inputVelocity = { 0.0f, 0.0f, 0.0f }; // 入力による目標速度を保持する変数
+    XMFLOAT3 m_desiredVelocity = { 0.0f, 0.0f, 0.0f }; // 目標速度を保持する変数
+
 public:
     /// @brief PlayerMoveMotorの移動処理を更新する
     void UpdateMove_Motor(struct PlayerMoveContext& context, const struct PlayerMoveIntent& intent, float deltaTime);
+
+private:
+    /// @brief 現在の状況に応じた平滑化時間を計算する
+    float CalculateSmoothTime(struct PlayerMoveContext& context);
+    /// @brief 目標位置を更新
+    void UpdateDesiredVelocity(struct PlayerMoveContext& context, float smoothTime, float deltaTime);
 
 };
