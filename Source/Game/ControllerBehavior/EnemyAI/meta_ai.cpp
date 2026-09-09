f19cc28a408a1188f6d1cdcb596e843e4aaddec6
amesira <miuktmr@gmail.com>
2026-09-10 01:49:28 +0900

    プレイヤー取得をMetaAIに記載

--- Source/Game/ControllerBehavior/EnemyAI/meta_ai.cpp
+++ Source/Game/ControllerBehavior/EnemyAI/meta_ai.cpp
@@ -5,17 +5,33 @@
 //===================================================
 #include "meta_ai.h"
 
+#include "Engine/Core/game_object.h"
+#include "Engine/Core/scene_interface.h"
+
+#include "Engine/Component/transform_component.h"
+
 void MetaAI::Initialize(const EnemyAIWorldContext& context)
 {
-    
+    IScene* scene = context.scene;
+
+    // プレイヤーの取得
+    m_player.gameObject = scene->GetGameObjectByName("Player");
+    if (m_player.gameObject) {
+        m_player.transform = m_player.gameObject->GetComponent<TransformComponent>();
+        m_player.entityInfo.gameObjectID = m_player.gameObject->GetID();
+    }
 }
 
 void MetaAI::Update(EnemyAIWorldContext& context, float deltaTime)
 {
-
+    if (m_player.transform) {
+        m_player.entityInfo.position = m_player.transform->GetPosition();
+        m_player.entityInfo.velocity = {};
+    }
 }
 
 void MetaAI::Finalize(const EnemyAIWorldContext& context)
 {
-    
+    m_player.gameObject = nullptr;
+    m_player.transform = nullptr;
 }
--- Source/Game/ControllerBehavior/EnemyAI/meta_ai.h
+++ Source/Game/ControllerBehavior/EnemyAI/meta_ai.h
@@ -7,16 +7,31 @@
 //---------------------------------------------------
 #ifndef META_AI_H
 #define META_AI_H
+#include <vector>
+#include <DirectXMath.h>
 #include "enemy_ai_world_context.h"
 
 // シーン内のプレイヤー・敵・戦況情報を共有する。
 class MetaAI {
 private:
+    struct PlayerInfo {
+        EnemyAiWorld::EntityInfo entityInfo;
+        class GameObject* gameObject = nullptr;
+        class TransformComponent* transform = nullptr;
+    };
+    struct EnemyInfo {
+        EnemyAiWorld::EntityInfo entityInfo;
+    };
+
+    PlayerInfo m_player;
+    std::vector<EnemyInfo> m_enemies;
     
 public:
     void Initialize(const EnemyAIWorldContext& context);
     void Update(EnemyAIWorldContext& context, float deltaTime);
     void Finalize(const EnemyAIWorldContext& context);
+
+    const DirectX::XMFLOAT3& GetPlayerPosition() const { return m_player.entityInfo.position; }
 
 };
 
